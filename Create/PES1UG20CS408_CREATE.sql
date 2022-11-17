0d8a4d4a8dcb0a785c301584117f3daa4e6ff227
Shreyas S <shreyas14902@gmail.com>
2022-11-17 23:12:11 +0530

    create

--- Create/PES1UG20CS408_CREATE.sql
+++ Create/PES1UG20CS408_CREATE.sql
@@ -1,10 +1,56 @@
 -- Creating the database:
-CREATE database dbms_mini_project;
+CREATE database mini_proj_408;
 -- Creating the tables:
-CREATE TABLE student(
-    roll_no int(10) NOT NULL,
-    name varchar(50) NOT NULL,
-    branch varchar(50) NOT NULL,
-    year int(10) NOT NULL,
-    PRIMARY KEY (roll_no)
+CREATE TABLE LEAGUE_408(
+league_id INT NOT NULL,
+league_name VARCHAR(20) NOT NULL,
+league_level INT NOT NULL,
+No_players INT NOT NULL,
+lvl_req INT NOT NULL,
+PRIMARY KEY(league_id)
 );
+CREATE TABLE GAME_SESSION_408(
+session_id INT NOT NULL,
+start_time TIME NOT NULL,
+No_players INT NOT NULL,
+server_region VARCHAR(20) NOT NULL,
+PRIMARY KEY(session_id)
+);
+
+CREATE TABLE CLAN_408(
+clan_id INT NOT NULL,
+no_members INT NOT NULL,
+lvl_req INT NOT NULL,
+PRIMARY KEY(clan_id)
+);
+--PLAYER ENTITY
+CREATE TABLE PLAYER_408(
+player_id INT NOT NULL,
+username VARCHAR(20) NOT NULL,
+clan_id INT NOT NULL,
+league_id INT NOT NULL,
+session_id INT NOT NULL,
+PRIMARY KEY(player_id),
+FOREIGN KEY(clan_id) REFERENCES CLAN_408(clan_id),
+FOREIGN KEY(league_id) REFERENCES LEAGUE_408(league_id),
+FOREIGN KEY(session_id) REFERENCES GAME_SESSION_408(session_id)
+);
+
+--SKILLS ENTITY
+CREATE TABLE SKILLS_408(
+    player_id INT NOT NULL,
+    level INT NOT NULL,
+    coins INT NOT NULL,
+    FOREIGN KEY(player_id) REFERENCES PLAYER_408(player_id)
+);
+-- Progress Entity
+CREATE TABLE PROGRESS_408(
+    player_id INT NOT NULL,
+    session_id INT NOT NULL,
+    trophies INT NOT NULL,
+    exp INT ,    
+    FOREIGN KEY(player_id) REFERENCES PLAYER_408(player_id),
+    
+);
+
+
